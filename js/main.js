7ad8c510fa928903b5813d73056d33c5ed78b077
Tanner Hart <tannerhart24@yahoo.com>
2023-01-19 11:36:07 -0800

    Created boilerplate template with basic player movement functionality

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const player = new Player();
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/backgroundLevel1.png'
+
+});
+let keys = {
+    space: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function init() {
+
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    backgroundLevel1.draw();
+
+
+    player.velocity.x = 0;
+
+    if (keys.d.pressed) player.velocity.x = 5;
+    else if (keys.a.pressed) player.velocity.x = -5;
+    else player.velocity.x = 0;
+
+    player.update();
+
+}
+
+init();
+animate();
