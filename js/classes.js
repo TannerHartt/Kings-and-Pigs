1383ae4705e58d3dffe22c39cb2e35d862b450ab
Tanner Hart <tannerhart24@yahoo.com>
2023-01-19 13:06:36 -0800

    Created vertical and horizontal collision detections and housed them in separate player object functions

--- js/classes.js
+++ js/classes.js
@@ -1,19 +1,21 @@
 class Player {
-    constructor() {
+    static collisionBuffer = 0.1;
+    constructor({ collisionBlocks = []}) {
         this.position = {
-            x: 100,
-            y: 100
+            x: 200,
+            y: 200
         }
         this.velocity = {
             x: 0,
             y: 0
         }
-        this.width = 100;
-        this.height = 100;
+        this.width = 25;
+        this.height = 25;
         this.sides = {
             bottom: this.position.y + this.height,
         }
         this.gravity = 1;
+        this.collisionBlocks = collisionBlocks;
     }
 
     draw() {
@@ -24,15 +26,70 @@ class Player {
     update() {
         this.draw();
 
-        this.position.y += this.velocity.y;
         this.position.x += this.velocity.x;
-        this.sides.bottom = this.position.y + this.height;
 
-        if (this.sides.bottom + this.velocity.y < canvas.height) {
-            this.velocity.y += this.gravity;
-        } else {
-            this.velocity.y = 0;
+        this.checkForHorizontalCollisions();
+
+        this.applyGravity();
+
+        this.checkForVerticalCollisions();
+
+    }
+
+    checkForHorizontalCollisions() {
+        // check horizontal collisions
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i];
+
+            // if a collision exists
+            if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height
+            ) {
+                // Collision on x-axis going to the left
+                if (this.velocity.x < 0) {
+                    this.position.x = collisionBlock.position.x + collisionBlock.width + Player.collisionBuffer
+                    break;
+                }
+                if (this.velocity.x > 0) {
+                    this.position.x = collisionBlock.position.x - this.width - Player.collisionBuffer
+                    break;
+                }
+            }
         }
+    }
+
+    checkForVerticalCollisions() {
+        // check for vertical collisions
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i];
+
+            // if a collision exists
+            if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x &&
+                this.position.y + this.height >= collisionBlock.position.y &&
+                this.position.y <= collisionBlock.position.y + collisionBlock.height
+            ) {
+                // Collision on y-axis
+                if (this.velocity.y < 0) {
+                    this.velocity.y = 0;
+                    this.position.y = collisionBlock.position.y + collisionBlock.height + Player.collisionBuffer
+                    break;
+                }
+                if (this.velocity.y > 0) {
+                    this.velocity.y = 0;
+                    this.position.y = collisionBlock.position.y - this.height - Player.collisionBuffer
+                    break;
+                }
+            }
+        }
+    }
+
+    applyGravity() {
+        // Apply gravity
+        this.velocity.y += this.gravity;
+        this.position.y += this.velocity.y;
     }
 }
 
@@ -50,5 +107,20 @@ class Sprite {
     draw() {
         if (!this.loaded) return;
         ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+class CollisionBlock {
+    static width = 64;
+    static height = 64;
+    constructor({ position }) {
+        this.position = position;
+        this.width = 64;
+        this.height = 64;
+    }
+
+    draw() {
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
