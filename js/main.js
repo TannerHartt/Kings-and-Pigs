47a6c75bceb48b315b8fa165ca23607de3d5ac50
Tanner Hart <tannerhart24@yahoo.com>
2023-01-19 16:28:21 -0800

    Began adding dynamic sprite animation.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,39 @@ const parsedCollisions = collisionsLevel1.parse2D();
 const collisionBlocks = parsedCollisions.createObjectsFrom2D()
 
 
-const player = new Player({ collisionBlocks });
+const player = new Player({
+    collisionBlocks,
+    imageSrc: './img/king/idle.png',
+    frameRate:  11,
+    animations: {
+        idleRight: {
+            frameRate: 11,
+            frameBuffer: 2,
+            loop: true,
+            imageSrc: './img/king/idle.png'
+        },
+        idleLeft: {
+            frameRate: 11,
+            frameBuffer: 2,
+            loop: true,
+            imageSrc: './img/king/idleLeft.png'
+
+        },
+        runRight: {
+            frameRate: 8,
+            frameBuffer: 2,
+            loop: true,
+            imageSrc: './img/king/runRight.png'
+        },
+        runLeft: {
+            frameRate: 8,
+            frameBuffer: 2,
+            loop: true,
+            imageSrc: './img/king/idleLeft.png'
+        }
+
+    }
+});
 const backgroundLevel1 = new Sprite({
     position: {
         x: 0,
