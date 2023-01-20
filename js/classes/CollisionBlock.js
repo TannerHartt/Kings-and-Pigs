cc877aca7df1a8e6b5524315421ebeab14a8dbea
Tanner Hart <tannerhart24@yahoo.com>
2023-01-20 09:11:47 -0800

    Abstracted each class into its own file

---
+++ js/classes/CollisionBlock.js
@@ -0,0 +1,14 @@
+class CollisionBlock {
+    static width = 64;
+    static height = 64;
+    constructor({ position }) {
+        this.position = position;
+        this.width = 64;
+        this.height = 64;
+    }
+
+    draw() {
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
