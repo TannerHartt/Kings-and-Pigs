cf87f2bd7a2a4f0cba3211951b1e52c94f7e3906
Tanner Hart <tannerhart24@yahoo.com>
2023-01-19 19:40:23 -0800

    Created level progression system. Fade-in fade-out animations. Created door animation handler. Further abstraction to come.

--- js/main.js
+++ js/main.js
@@ -4,13 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-const parsedCollisions = collisionsLevel1.parse2D();
-
-const collisionBlocks = parsedCollisions.createObjectsFrom2D()
-
-
 const player = new Player({
-    collisionBlocks,
     imageSrc: './img/king/idle.png',
     frameRate:  11,
     animations: {
@@ -38,30 +32,149 @@ const player = new Player({
             frameBuffer: 4,
             loop: true,
             imageSrc: './img/king/runLeft.png'
+        },
+        enterDoor: {
+            frameRate: 8,
+            frameBuffer: 4,
+            loop: false,
+            imageSrc: './img/king/enterDoor.png',
+            onComplete: () => {
+                console.log('Completed Animation');
+                gsap.to(overlay, {
+                    opacity: 1,
+                    onComplete: () => {
+                        level++;
+
+                        if (level === 4) level = 1;
+                        levels[level].init();
+                        player.switchSprite('idleRight');
+                        player.preventInput = false;
+
+                        gsap.to(overlay, {
+                            opacity: 0,
+                            doors: [],
+                        })
+                    }
+                })
+            }
         }
     }
 });
 
-const doors = [
-    new Sprite({
-        position: {
-            x: 757,
-            y: 273
-        },
-        imageSrc: './img/doorOpen.png',
-        frameRate: 5,
-        frameBuffer: 8,
-        loop: false,
-        autoPlay: false
-    })
-]
-const backgroundLevel1 = new Sprite({
-    position: {
-        x: 0,
-        y: 0
+
+let backgroundLevel;
+let parsedCollisions;
+let collisionBlocks;
+let doors = [];
+
+
+let level = 1;
+let levels = {
+    1: {
+        init: () => {
+            parsedCollisions = collisionsLevel1.parse2D();
+            collisionBlocks = parsedCollisions.createObjectsFrom2D();
+            player.collisionBlocks = collisionBlocks;
+
+            if (player.currentAnimation) {
+                player.currentAnimation.isActive = false;
+            }
+            backgroundLevel = new Sprite({
+                position: {
+                    x: 0,
+                    y: 0
+                },
+                imageSrc: './img/backgroundLevel1.png'
+            });
+            doors = [
+                new Sprite({
+                    position: {
+                        x: 757,
+                        y: 273
+                    },
+                    imageSrc: './img/doorOpen.png',
+                    frameRate: 5,
+                    frameBuffer: 8,
+                    loop: false,
+                    autoPlay: false
+                })
+            ];
+        }
     },
-    imageSrc: './img/backgroundLevel1.png'
-});
+
+    2: {
+        init: () => {
+            parsedCollisions = collisionsLevel2.parse2D();
+            collisionBlocks = parsedCollisions.createObjectsFrom2D();
+            player.collisionBlocks = collisionBlocks;
+            player.position.x = 96;
+            player.position.y = 140;
+
+            if (player.currentAnimation) {
+                player.currentAnimation.isActive = false;
+            }
+
+            backgroundLevel = new Sprite({
+                position: {
+                    x: 0,
+                    y: 0
+                },
+                imageSrc: './img/backgroundLevel2.png'
+            });
+            doors = [
+                new Sprite({
+                    position: {
+                        x: 772.00,
+                        y: 336
+                    },
+                    imageSrc: './img/doorOpen.png',
+                    frameRate: 5,
+                    frameBuffer: 8,
+                    loop: false,
+                    autoPlay: false
+                })
+            ];
+        }
+    },
+
+    3: {
+        init: () => {
+            parsedCollisions = collisionsLevel3.parse2D();
+            collisionBlocks = parsedCollisions.createObjectsFrom2D();
+            player.collisionBlocks = collisionBlocks;
+            player.position.x = 734;
+            player.position.y = 209;
+
+            if (player.currentAnimation) {
+                player.currentAnimation.isActive = false;
+            }
+            backgroundLevel = new Sprite({
+                position: {
+                    x: 0,
+                    y: 0
+                },
+                imageSrc: './img/backgroundLevel3.png'
+            });
+            doors = [
+                new Sprite({
+                    position: {
+                        x: 175,
+                        y: 333
+                    },
+                    imageSrc: './img/doorOpen.png',
+                    frameRate: 5,
+                    frameBuffer: 8,
+                    loop: false,
+                    autoPlay: false
+                })
+            ];
+        }
+    }
+
+
+}
+
+
 let keys = {
     space: {
         pressed: false
@@ -74,45 +187,34 @@ let keys = {
     },
 }
 
-function init() {
-
+const overlay = {
+    opacity: 0
 }
 
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    backgroundLevel1.draw();
+    backgroundLevel.draw();
 
-    collisionBlocks.forEach(collisionBlock => {
-        collisionBlock.draw();
-    });
+    // collisionBlocks.forEach(collisionBlock => {
+    //     collisionBlock.draw();
+    // });
 
     doors.forEach(door => {
         door.draw();
     });
 
-    player.velocity.x = 0;
 
-    if (keys.d.pressed) {
-        player.switchSprite('runRight');
-        player.velocity.x = 5;
-        player.lastDirection = 'right';
-    }
-    else if (keys.a.pressed) {
-        player.switchSprite('runLeft');
-        player.velocity.x = -5;
-        player.lastDirection = 'left';
-    }
-    else {
-        if (player.lastDirection === 'left') player.switchSprite('idleLeft');
-        else player.switchSprite('idleRight');
-
-        player.velocity.x = 0;
-    }
-
+    player.handleInput(keys);
     player.update();
+
+    ctx.save();
+    ctx.globalAlpha = overlay.opacity;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
 
 }
 
-init();
+levels[level].init();
 animate();
